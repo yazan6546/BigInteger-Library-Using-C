d34e916cf66de14d6af03ccafc180eb449c580ff
yazan AbuAloun <61243898+yazan6546@users.noreply.github.com>
2024-06-10 00:49:28 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,19 +9,6 @@
 
 void showMenu ();
 struct node** readFile1 (int *size);
-struct node* addNumbers (struct node *l1, struct node *l2);
-struct node* subtractNumbers (struct node *l1 , struct node *l2);
-struct node **divideAndRemainder (struct node *l1, struct node *l2);
-void moveDigitsSum (struct node* L, struct node* sumList, struct node* digit, int carry);
-void moveDigitsSub (struct node *L, struct node* sumList, struct node* digit);
-struct node* multiplyListByNumber (struct node *l1, int number);
-struct node* multiplyNumbers (struct node *l1, struct node *l2);
-int compare (struct node *l1, struct node *l2);
-void borrow (struct node *l1, struct node *digit1);
-void filterList (struct node *l1);
-struct node * createZero();
-void printNumber(struct node *L, FILE *q);
-int divisionSubtract (struct node *l1, struct node *l2);
 void delete_array_linkedLists(struct node **array, int length);
 
 int main() {
